636f354c35c8c6ef695fa7339cf7362bd3751a6f
Nick Robinson <nrobinson295@gmail.com>
2017-09-11 21:54:11 -0400

    built in console.log

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-	alert('new');
+	for (i = 1; i <= 100; i++) {
+
+		if (i % 3 === 0 && i % 5 === 0){
+			console.log('fizzbuzz');
+		}else if (i % 3 === 0){
+			console.log('fizz');
+		}else if (i % 5 === 0){
+			console.log('buzz');
+		}else {
+			console.log(i);
+		}
+	}
 });
