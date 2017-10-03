ca3188fd87412e7c2fa09483ad2325a9493ec05c
Nick Robinson <nrobinson295@gmail.com>
2017-10-02 20:11:22 -0400

    new version

--- app.js
+++ app.js
@@ -1,20 +1,14 @@
 $(document).ready(function() {
 	function showToPage(message) {
 		$('body').append('<p>' + message + '</p>');
+		
 	}
+
 
 
 	for (i = 1; i <= 100; i++) {
 
-		if (i % 3 === 0 && i % 5 === 0){
-			showToPage('FizzBuzz');
-		}else if (i % 3 === 0){
-			showToPage('Fizz');
-		}else if (i % 5 === 0){
-			showToPage('Buzz');
-		}else {
-			showToPage(i);
-		}
+		
 	}
 });
 
