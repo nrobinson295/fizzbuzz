2cebf573a310580c99d7d092613ef189c03cf338
Nick Robinson <nrobinson295@gmail.com>
2017-10-05 20:33:46 -0400

    fizzbuzz challange completed

--- app.js
+++ app.js
@@ -1,5 +1,35 @@
 $(document).ready(function() {
-	prompt('supply a number for fizzbuzz');
+	//prompt('supply a number for fizzbuzz');
+	var x = prompt('supply a number for fizz');
+	var y = prompt('supply a number for buzz');
+	validateInput(x, y);
+	//showToPage(value);
+	fizzBuzz(x, y);
+		
+
+	function fizzBuzz(fizzNumber, buzzNumber){
+		for (i = 1; i <= 100; i++) {
+			if (i % fizzNumber === 0 && i % buzzNumber === 0){
+				showToPage('FizzBuzz');
+			}else if (i % fizzNumber === 0){
+				showToPage('Fizz');
+			}else if (i % buzzNumber === 0){
+				showToPage('Buzz');
+			}else {
+				showToPage(i);
+			}
+		}	
+	}
+	function validateInput(xString, yString){
+		x = parseInt(xString);
+		y = parseInt(yString);
+		if (isNaN(x)){
+			showToPage('Fizz is not a number');
+		}
+		if (isNaN(y)){
+			showToPage('Buzz is not a number');
+		}	
+	}
 
 
 
@@ -10,10 +40,7 @@ $(document).ready(function() {
 
 
 
-	for (i = 1; i <= 100; i++) {
-		showToPage(i);
-		
-	}
+	
 });
 
 
